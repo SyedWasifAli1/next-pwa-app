23c8a70eaa88e82241c1422a011e74b353e8c361
SyedWasifAli1 <syedwasifali080@gmail.com>
2026-01-20 04:47:37 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const pwaConfig = withPWA({
    dest: 'public',       // service worker output
     register: true,       // auto-register service worker
     skipWaiting: true,    // immediately activate new SW
-  disable: process.env.NODE_ENV === 'development',
+  // disable: process.env.NODE_ENV === 'development',
 });
 
 export default pwaConfig(nextConfig);
